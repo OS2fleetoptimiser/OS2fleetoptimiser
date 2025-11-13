c36fe4f9465eaea2a5755b060762e99df0fd8af8
Andreas Bisgaard <andreas@droidsagency.com>
2025-11-13 20:36:38 +0100

    implement #191: remove co2(g)/km field

--- fleetoptimiser-frontend/app/(logged-in)/configuration/ValidationScheme.tsx
+++ fleetoptimiser-frontend/app/(logged-in)/configuration/ValidationScheme.tsx
@@ -5,7 +5,6 @@ export const validationSchema = Yup.object().shape({
     make: Yup.string().notRequired(),
     model: Yup.string().notRequired(),
     capacity_decrease: Yup.number().nullable().notRequired(),
-    co2_pr_km: Yup.number().nullable().notRequired(),
     range: Yup.number().nullable().notRequired(),
     omkostning_aar: Yup.number().nullable().notRequired(),
     department: Yup.string().notRequired(),
